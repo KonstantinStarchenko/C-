a8c6bc8ddee3a8203282746b4d1c98daf451d7ba
Kostya <optics76@yandex.ru>
2023-03-06 15:03:33 +0300

    add Example008 Dog and Two Frands

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count, distance, firstFriendSpeed, secondFriendSpeed, time, dogSpeed, friend;
+
+count = 0;
+distance = 10000;
+firstFriendSpeed = 2;
+secondFriendSpeed = 1;
+dogSpeed = 5;
+friend = 2;
+time = 0;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = (int)((double)distance / (firstFriendSpeed + dogSpeed));
+        friend = 2;
+    }
+    else
+    {
+        time = (int)((double)distance / (secondFriendSpeed + dogSpeed));
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+Console.WriteLine(count);
